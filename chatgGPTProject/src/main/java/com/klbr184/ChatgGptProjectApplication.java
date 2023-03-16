fe0d1a5754bae1602af46a91ec8384dbab8fd075
KLBR184 <2640477833@qq.com>
2023-03-16 19:33:43 +0800

    登录页面与后端创建

---
+++ chatgGPTProject/src/main/java/com/klbr184/ChatgGptProjectApplication.java
@@ -0,0 +1,13 @@
+package com.klbr184;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgGptProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatgGptProjectApplication.class, args);
+    }
+
+}
---
+++ chatgGPTProject/src/test/java/com/klbr184/ChatgGptProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klbr184;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgGptProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
